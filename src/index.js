b2e732bbf36a116a2932a6607bf45780390c3517
Luong <luongvh@nextgsolutions.com>
2024-05-21 14:47:45 +0700

    test

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from './contexts/auth/AuthContext';
+import AuthProvider from 'react-auth-kit/AuthProvider';
+import { store } from './provider/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <AuthProvider store={store}>
         <App />
       </AuthProvider>
     </BrowserRouter>
--- src/modules/auth/sign-in/index.tsx
+++ src/modules/auth/sign-in/index.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 // import { useAuth } from "../../../hook/useAuth";
 // import { authService } from "../../../hook/useAuth";
 // import { signIn } from "../../../contexts/auth/reducers";
+import useSignIn from 'react-auth-kit/hooks/useSignIn';
+import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
 
 const SignIn: FC = () => {
   // const { dispatch } = useAuth();
@@ -16,10 +18,33 @@ const SignIn: FC = () => {
   //   }
   // }
 
+  const signIn = useSignIn();
+  const auth = useAuthUser()
+  const onSubmit = (e: any) => {
+    if (signIn({
+      auth: {
+        token: 'ey....mA',
+        type: 'Bearer'
+      },
+      refresh: 'ey....mA',
+      userState: {
+        name: 'React User',
+        uid: 123456
+      }
+    })) {
+      // Redirect or do-something
+    } else {
+      //Throw error
+    }
+  }
+
+  const handle = () => console.log(auth)
+
   return (
     <>
       <div>Sign In</div>
-      {/* <button onClick={handleSignIn}>...</button> */}
+      <button onClick={onSubmit}>Login</button>
+      <button onClick={handle}>Handle</button>
     </>
   );
 };
