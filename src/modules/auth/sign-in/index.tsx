3f9e7454c1a8e11c92ee9d0327756759b6fb4eac
Luong <luongvh@nextgsolutions.com>
2024-05-22 17:18:48 +0700

    Update index.tsx

--- src/modules/auth/sign-in/index.tsx
+++ src/modules/auth/sign-in/index.tsx
@@ -2,12 +2,13 @@ import { FC } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const SignIn: FC = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, loginWithPopup } = useAuth0();
 
   return (
     <>
       <div>Sign In</div>
       <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button onClick={() => loginWithPopup()}>Sign Up</button>
     </>
   );
 };
